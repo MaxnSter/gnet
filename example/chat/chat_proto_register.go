d6bfd31164727b1b819498e9d4423d1f241a5274
maxnster <maxnster20@gmail.com>
2018-06-14 11:03:27 +0800

    add example chat

---
+++ example/chat/chat_proto_register.go
@@ -0,0 +1,18 @@
+package chat
+
+import (
+	"reflect"
+
+	"github.com/MaxnSter/gnet/message_pack/message_meta"
+)
+
+const (
+	// 消息id保证唯一
+	ChatMsgId = 0
+)
+
+func init() {
+	// 注册该id对应的类型信息,我们使用tlv Packer时可以根据解析的id反射生成对应类型的struct
+	metaType := reflect.TypeOf((*ChatMessage)(nil)).Elem()
+	message_meta.RegisterMsgMeta(&message_meta.MessageMeta{ChatMsgId, metaType})
+}
---
+++ example/chat/server/chat_server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net"
+
+	// 注册使用的组件
+	_ "github.com/MaxnSter/gnet/codec/codec_protobuf"
+	_ "github.com/MaxnSter/gnet/message_pack/pack/pack_type_length_value"
+	_ "github.com/MaxnSter/gnet/net/tcp"
+	_ "github.com/MaxnSter/gnet/worker_pool/worker_session_race_self"
+
+	"github.com/MaxnSter/gnet"
+	"github.com/MaxnSter/gnet/example/chat"
+)
+
+func main() {
+	m := gnet.NewModule(gnet.WithPacker("tlv"),
+		gnet.WithCoder("protobuf"),
+		gnet.WithPool("poolRaceSelf"))
+
+	o := gnet.NewOperator(onMessage)
+	o.SetOnConnected(onConnected)
+
+	server := gnet.NewNetServer("tcp", "chat", m, o)
+	server.ListenAndServe(":2007")
+}
+
+func onMessage(ev gnet.Event) {
+	switch msg := ev.Message().(type) {
+	case *chat.ChatMessage:
+		ev.Session().AccessManager().Broadcast(func(session gnet.NetSession) {
+			session.Send(msg)
+		})
+	default:
+		panic("error msg type")
+	}
+}
+
+func onConnected(session gnet.NetSession) {
+	msg := &chat.ChatMessage{
+		Id:     chat.ChatMsgId,
+		Talker: session.Raw().(net.Conn).LocalAddr().String(),
+		Msg:    "welcome",
+	}
+
+	session.AccessManager().Broadcast(func(s gnet.NetSession) {
+		s.Send(msg)
+	})
+}
